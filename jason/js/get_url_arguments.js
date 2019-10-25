80b08ba95e6757e713e7a79247b686b25d8bc0a2
liuhan <liuhan@hqwx.com>
2019-10-25 14:52:38 +0800

    添加function

--- jason/js/get_url_arguments.js
+++ jason/js/get_url_arguments.js
@@ -4,3 +4,10 @@ function getQueryString(name) {
 	if(r != null) return unescape(r[2]);
 	return null;
 };
+
+function getQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+	var r = window.location.search.substr(1).match(reg);
+	if(r != null) return unescape(r[2]);
+	return null;
+};
